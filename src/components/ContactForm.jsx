21623f270d0edca5dc28514f18741f43ef069536
Bassim Durzi <bassim.ali.durzi@gmail.com>
2024-11-15 19:46:22 +0100

    Send data from Contact Form to Api

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,17 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    specialist: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleOk = () => {
+    setSubmitted(false)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch(
+      "https://win24-assignment.azurewebsites.net/api/forms/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+
+    if (res.ok) {
+      setSubmitted(true);
+      setFormData({
+        fullName: "",
+        email: "",
+        specialist: "",
+      });
+    }
+  };
+
+  if (submitted) {
+    return (
+      <div className="informationBox">
+        <h1>Thank you for your message!</h1>
+        <p>We will contact you as soon as possible.</p>
+        <button onClick={handleOk}>Ok</button>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="contact-form">
         <h3>Get Online Consultation</h3>
-        <form action="" className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <label htmlFor="fullName">Full name</label>
-          <input type="text" id="fullName" />
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+            placeholder="Full Name"
+          />
+
           <label htmlFor="email">Email address</label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            placeholder="Email"
+          />
+
           <label htmlFor="specialist">Specialist</label>
-          <input type="text" id="specialist" />
+          <input
+            type="text"
+            id="specialist"
+            name="specialist"
+            value={formData.specialist}
+            onChange={handleChange}
+            required
+            placeholder="Specialist"
+          />
           <input id="submit" type="submit" value="Make an appointment" />
         </form>
       </div>
